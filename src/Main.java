875810183f0f5184bfca13cab03ab1fc245f70d4
Nikolay <masknik1666@yandex.ru>
2023-11-23 05:29:36 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 6000;//  стоимость билета
+        int rub = 20;// рублей за 1 милю
+        int bonus = (ticket / rub); //
+        System.out.println(bonus);
+
+    }
+}
